75a14d3683d41da9f2f388e6132ce08070493326
ankireddy1 <101354717+ankireddy1@users.noreply.github.com>
2022-06-16 18:27:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,17 @@
 
+def newGit(repo)
+{
+  git "${repo}"
+}
+def Maven()
+{
+  sh 'mvn pakage'
+}
+def Deploy(jobname,ip,app)
+{
+  sh 'scp /home/ubuntu/.jenkins/workspace/"${jobname}"/webapp/target/webapp.war ubuntu@"${ip}":/var/lib/tomcat9/"${app}".war'
+}
+def Test()
+{
+  sh 'java -jar /home/ubuntu/.jenkins/workspace/"${jobname}"/testing.jar'
+}
